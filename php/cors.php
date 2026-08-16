664534ff9ac6564bb733c5dd8064088efc401f19
JonatanRamirez <jonaram727@gmail.com>
2026-08-15 23:20:24 -0300

    fix: Configurar CORS estricto para Vercel

--- php/cors.php
+++ php/cors.php
@@ -3,7 +3,7 @@
 
 // Permitir solicitudes desde cualquier origen (Para desarrollo/Vercel)
 // TODO: En producción, cambia '*' por 'https://tudominio.vercel.app'
-header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Origin: https://camping-control-9kdh.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Access-Control-Allow-Credentials: true");
